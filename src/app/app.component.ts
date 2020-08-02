eab0f54b664032e5a5663af9f005c9d28c3762d6
Maury Areas <mareasperez7@gmail.com>
2020-08-01 18:17:26 -0600

    fin del crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Empleado } from './models/empleado';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-anguar-crud';
+  EmpleadoSeleccionado: Empleado = new Empleado();
+  EmpleadoArray: Empleado[] = [
+    { id: 1, name: 'Ryan', country: 'USA' },
+    { id: 2, name: 'Angelica', country: 'USA' },
+    { id: 3, name: 'Joe', country: 'USA' }
+  ];
+  AddOREdit() {
+    if (this.EmpleadoSeleccionado.id == 0) {
+      this.EmpleadoSeleccionado.id = this.EmpleadoArray.length + 1;
+      this.EmpleadoArray.push(this.EmpleadoSeleccionado);
+
+    }
+    this.EmpleadoSeleccionado = new Empleado();
+  }
+  Edit(empleado: Empleado) {
+    this.EmpleadoSeleccionado = empleado;
+  }
+
+  delete() {
+    if (confirm('estas seguro de querer eliminar?')) {
+      this.EmpleadoArray = this.EmpleadoArray.filter(emp => emp !== this.EmpleadoSeleccionado);
+      this.EmpleadoSeleccionado = new Empleado()
+    }
+  }
+
 }
---
+++ src/app/models/empleado.ts
@@ -0,0 +1,6 @@
+export class Empleado {
+id: number=0;
+name: string;
+country: string;
+}
+
